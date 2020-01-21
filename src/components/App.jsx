d5d1b6755fc5fc70f98b91bf92b60f257bed787b
Naveen Sakthivel <naveenrobotics@live.in>
2020-01-21 16:46:32 +0530

    Virtual DOM

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Timer from './Timer';
+
+function App() {
+    return (
+        <div>
+            <h1>hello</h1>
+            <Timer />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Timer() {
+    return (
+        <div>
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+        </div>
+    )
+}
+
+export default Timer;
