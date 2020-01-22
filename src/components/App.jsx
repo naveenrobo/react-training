db437af2e53283a18725e62dfbbb3c9d9f7d3d65
Naveen Sakthivel <naveenrobotics@live.in>
2020-01-22 16:38:10 +0530

    state and lifecyle part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import Comment from './Comment';
-
-const abc = {
-    avatarUrl: "https://www.gravatar.com/avatar/00000000000000000000000000000000?s=50",
-    name: "Naveen Sakthivel"
-}
-
 function App() {
     return (
         <div>
-            <Comment author={abc} text="My Comment" date="2020-01-22" />
-            
+            test
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            count: 1
+        }
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+
+        this.setState({ date: new Date() })
+
+        this.setState((state, props) => ({ count: state.count + 1 }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count : {this.state.count}</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(
-    <App />,
+    <Clock />,
     document.getElementById('root')
 );
